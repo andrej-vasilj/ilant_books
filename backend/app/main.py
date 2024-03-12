6c03f46969daccd6a7ca128580b6d36e92db3c37
Andrej Vasilj <andrej_vasilj@hotmail.com>
2024-03-12 14:23:02 -0400

    Added skeleton nextjs project and a service for sending requests to our BE

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,32 @@
 from os import getenv
 from typing import Optional
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
-
-app = FastAPI()
 API_KEY = getenv('API_KEY')
 GOOGLE_URL = getenv('GOOGLE_URL')
 
+# Setup the FastAPI server and allow CORS for local testing with next.js FE
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# End points
 @app.get("/search/")
 def search(query_string: str, start_index: Optional[int] = 0):
     r = requests.get(f'{GOOGLE_URL}?q={query_string}&startIndex={start_index}&key={API_KEY}')
---
+++ frontend/nextjs-books/app/lib/bookService.tsx
@@ -0,0 +1,29 @@
+import { Subject } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { map, catchError, of } from 'rxjs';
+
+
+class BookService {
+	bookResults: Subject = new Subject();
+
+	search(query: string, startIndex: number = 0) {
+		console.log('Querying books for: ' + query);
+		ajax.getJSON('http://0.0.0.0/search/?query_string=' + query + '&start_index=' + startIndex).subscribe({
+		  next: value => {
+		  	console.log(value);
+		  	this.bookResults.next(value);
+		  },
+		  error: err => {
+		  	console.log(err);
+		  	this.bookResults.next(err.detail)
+		  }
+		});
+	}
+}
+
+
+// Not sure what the best way of achieving a singleton service is with react/next.js, but
+// I'll try exporting an instance of the class here. Angular would have dependency injection take care of this
+// for me
+const bookService = new BookService();
+export default bookService;
