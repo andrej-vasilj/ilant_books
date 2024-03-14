117339eaa5116f2d1807705281f0e0428b7c25d5
Andrej Vasilj <andrej_vasilj@hotmail.com>
2024-03-14 12:29:17 -0400

    Nicer detail page styling

--- frontend/nextjs-books/app/books/[id]/page.tsx
+++ frontend/nextjs-books/app/books/[id]/page.tsx
@@ -23,30 +23,33 @@ export default function Detail({ params }: { params: { id: string } }) {
 	}, [params.id]);
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-1">
-	      <div className="w-full max-w-6xl h-full">
+		<main className="flex min-h-screen flex-col items-center justify-between p-2">
+	      <div className="w-full max-w-6xl h-full mx-auto rounded-xl shadow-md p-5 border-2 border-rich-green bg-white">
+
 	      	<Link key="home" href="/" className="float-left">
 	      		<button type="button" className="bg-dark-green p-2 rounded-xl shadow-md">
-		    		<ArrowLeftIcon className="h-[25px] w-[25px] float-left mr-2 text-white" /> <span className="font-bold text-white"> Back </span>
-		    	</button>
-		    </Link>
+			    		<ArrowLeftIcon className="h-[25px] w-[25px] float-left mr-2 text-white" /> <span className="font-bold text-white"> Back </span>
+			    	</button>
+			    </Link>
+
 	        <div className="w-full items-start justify-between font-mono flex p-1 h-full">
 	          <Image
-		        src={book.volumeInfo?.imageLinks?.thumbnail || '/no_preview2.png'}
-		        alt="No Preview"
-		        height="100"
-		        width="100"
-		        className="rounded object-contain w-40 h-auto mt-4"
-		      />
-		      <div className="min-w-0 flex-auto w-full p-3">
-		      	<div className="shrink-0 flex flex-col justify-between">
-		      		<p className="text-lg font-bold leading-6 text-gray-900">{book.volumeInfo?.title}</p>
-		      		<p className="mt-1 truncate text-md leading-5 text-gray-700"><b>Authors: </b> {book.volumeInfo?.authors.join(', ')}</p>
-		      		<p className="mt-1 text-md leading-5 text-gray-500"><b>Published: </b> <time dateTime="2023-01-23">{book.volumeInfo?.publishedDate}</time></p>
-		      		<p className="mt-1 text-sm leading-5 text-gray-500"><b>Description: </b>{book.volumeInfo?.description}</p>
-		      	</div>
-		      </div>
+			        src={book.volumeInfo?.imageLinks?.thumbnail || '/no_preview2.png'}
+			        alt="No Preview"
+			        height="100"
+			        width="100"
+			        className="rounded object-contain w-40 h-auto mt-4"
+			      />
+			      <div className="min-w-0 flex-auto w-full p-3">
+			      	<div className="shrink-0 flex flex-col justify-between">
+			      		<p className="text-lg font-bold leading-6 text-gray-900">{book.volumeInfo?.title}</p>
+			      		<p className="mt-1 truncate text-md leading-5 text-gray-700"><b>Authors: </b> {book.volumeInfo?.authors.join(', ')}</p>
+			      		<p className="mt-1 text-md leading-5 text-gray-500"><b>Published: </b> <time dateTime="2023-01-23">{book.volumeInfo?.publishedDate}</time></p>
+			      		<p className="mt-1 text-sm leading-5 text-gray-500"><b>Description: </b>{book.volumeInfo?.description}</p>
+			      	</div>
+			      </div>
 	        </div>
+
 	      </div>
 	    </main>
 	);
